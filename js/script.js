805b1268f87186b283fbe8cf5565e600494655e9
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-01-22 17:06:35 +0530

    commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+console.log("A");
+console.log("B");
+
+// for (let i = 0; i < 1000000000000; i++) {
+//   // Simulating a time-consuming task
+//   let a=1;
+// }
+function sum(){
+console.log("C");    
+    return 1+2;
+}
+setTimeout(sum, 5000);
+// let ab = setInterval(sum, 1000);
+
+console.log("D");
+console.log("E");
