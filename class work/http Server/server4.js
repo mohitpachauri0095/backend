71094230ed1dbcba9500c72b627f2d49fd98fbea
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-02-13 14:08:43 +0530

    commit

--- class work/http Server/server4.js
+++ class work/http Server/server4.js
@@ -19,7 +19,7 @@ const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "application/json" });
     res.end(JSON.stringify(userObject));
   }
-
+ 
 
   else if (req.method === "POST" && req.url === "/user") {
 
