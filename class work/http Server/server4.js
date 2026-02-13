fb088133b0ce74bcbc339f2329e8088dfbb26203
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-02-13 14:09:00 +0530

    commit

--- class work/http Server/server4.js
+++ class work/http Server/server4.js
@@ -27,7 +27,7 @@ const server = http.createServer((req, res) => {
 
     req.on("data", (chunk) => {
       body += chunk;
-    });
+    }); 
 
     req.on("end", () => {
       fs.writeFile("data.txt", body, () => {
