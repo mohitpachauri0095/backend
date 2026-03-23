13c2a3e0dd54aab4fc0acc189e8fb077fa90cc8d
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-03-23 20:26:15 +0530

    commit

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ console.log("B");
 // for (let i = 0; i < 1000000000000; i++) {
 //   // Simulating a time-consuming task
 //   let a=1;
-// }
+// }    
 function sum(){        
 console.log("C");      
     return 1+2;      
