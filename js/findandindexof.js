f5d8b0661951476a3797c07fde032a606dcc801e
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-04-25 15:54:22 +0530

    commit

--- js/findandindexof.js
+++ js/findandindexof.js
@@ -1,6 +1,7 @@
 var arr =[1,2,3,4];
 
-arr.find(function(val){
-if (val==2)return val;
-})
-console.log(val); 
+var result = arr.find(function (val) {
+  return val == 2;
+});
+
+console.log(result);
--- js/script.js
+++ js/script.js
@@ -1,16 +1,27 @@
-console.log("A");
-console.log("B");   
+// console.log("A");
+// console.log("B");   
 
-// for (let i = 0; i < 1000000000000; i++) {
-//   // Simulating a time-consuming task
-//   let a=1;
-// }    
-function sum(){         
-console.log("C");        
-    return 1+2;      
-}                                              
-setTimeout(sum, 5000); 
-// let ab = setInterval(sum, 1000);
+// // for (let i = 0; i < 1000000000000; i++) {
+// //   // Simulating a time-consuming task
+// //   let a=1;
+// // }    
+// function sum(){         
+// console.log("C");        
+//     return 1+2;      
+// }                                              
+// setTimeout(sum, 5000); 
+// // let ab = setInterval(sum, 1000);
 
-console.log("D");
-console.log("E");    
+// console.log("D");
+// console.log("E");    
+
+
+// const fs = require("fs");
+
+// fs.writeFile("hie.text", "hello who are you brother", (err) => {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     console.log("done");
+//   }
+// });
