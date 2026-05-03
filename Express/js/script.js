95a12a59ed42342a6d98b32754127fa2665a7c61
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-05-03 18:02:11 +0530

    commit

---
+++ Express/js/script.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+app.get("/", function(req, res){
+    res.send("champions meraa mohiji or ");
+})
+
+app.get("/profile", function(req, res){
+    res.send("champion uska tu ");
+})
+
+app.listen(3000);
