c3e70c534eaf4384f793db56284dd3a7b48847ec
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-06-06 12:07:44 +0530

    commit

--- js/server.js
+++ js/server.js
@@ -3,7 +3,7 @@ const server = http.createServer((req, res) => {
   res.write("Hello mohit server is running now");
   
   res.end();
-});
+});   
 server.listen(3000, () => {
     console.log("server running at https://localhost:3000/");
 });
