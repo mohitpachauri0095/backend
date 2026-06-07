2a9a6c9dfef414628200e100501c782a95ef9e71
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-06-07 21:43:34 +0530

    commit

--- Express/index.js
+++ Express/index.js
@@ -7,7 +7,7 @@
 // });
      
 const express = require('express');
-const app = express();
+const app = express();   
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));   
