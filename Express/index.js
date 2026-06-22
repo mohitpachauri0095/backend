1d3731bffee6ff6dfd1541b09c290e21291dfdc6
mohitpachauri0095 <mohitpachauri095@gmail.com>
2026-06-22 23:05:06 +0530

    commit

--- Express/index.js
+++ Express/index.js
@@ -1,18 +1,4 @@
-// const app = require('./src/aap');
-
-// const PORT = 3000;
-
-// app.listen(PORT, () => {
-//     console.log(`Server is running on port ${PORT}`);
-// });
-     
 const express = require('express');
-const app = express();   
+const app = express();
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));   
-
-app.get("/", function(req,res){
-      
-});      
-
+app.use(express.static(Path2D.join(__dirname, '')))   
